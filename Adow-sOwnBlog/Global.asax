4c4eeb1e9d32ba2570b4913766a64078bbcf8c43
Matheus Assis <adowtatep@hotmail.com.br>
2016-01-09 13:38:33 -0200

    Project Created
    The project has been initiated

---
+++ Adow-sOwnBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Adow_sOwnBlog.MvcApplication" Language="C#" %>
---
+++ Adow-sOwnBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Adow_sOwnBlog.Startup))]
+namespace Adow_sOwnBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
