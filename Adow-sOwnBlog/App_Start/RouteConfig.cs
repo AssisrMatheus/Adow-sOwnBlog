3882dabd1df02e73e9e2263ad45ffeb97962bfed
Matheus Assis <adowtatep@hotmail.com.br>
2016-01-10 16:48:47 -0200

    Routes working, partly everything working
    The only thing that isn't implemented is the user panel. Creating a
    user, posting an article etc

--- Adow-sOwnBlog/App_Start/RouteConfig.cs
+++ Adow-sOwnBlog/App_Start/RouteConfig.cs
@@ -14,6 +14,24 @@ namespace Adow_sOwnBlog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "All articles",
+                url: "articles/",
+                defaults: new  { controller = "Home", action = "Index" }
+            );
+
+            routes.MapRoute(
+                name: "Category Specific",
+                url: "articles/{category}",
+                defaults: new { controller = "Home", action = "showCategory", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Article Specific",
+                url: "articles/{category}/{title}",
+                defaults: new { controller = "Home", action = "showArticle" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- Adow-sOwnBlog/Controllers/HomeController.cs
+++ Adow-sOwnBlog/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace Adow_sOwnBlog.Controllers
     {
         //Create a simple test category for those articles
         private List<ArticleCategory> categories;
+        private List<ArticleCategory> articleCategories;
 
         private List<Article> articles;
 
@@ -19,23 +20,32 @@ namespace Adow_sOwnBlog.Controllers
         {
             //Create a simple test author
             Author test = new Author(1, "Test", "test@test.com", "testpass");
-            
-            //Create categories to add on those news
+
+            //Instantiate all categories list
             categories = new List<ArticleCategory>();
-            categories.Add(new ArticleCategory(1, "Mexico"));
-            categories.Add(new ArticleCategory(2, "Prision"));
+
+            //Create categories only related to this artcle(used for all of them only for test purposes)
+            articleCategories = new List<ArticleCategory>();
+            articleCategories.Add(new ArticleCategory(1, "Mexico"));
+            articleCategories.Add(new ArticleCategory(2, "Prision"));
+
+            //Adds the recent created categories in the all categories List
+            foreach (ArticleCategory category in articleCategories){
+                categories.Add(category);
+            }
 
             //Create Articles
             articles = new List<Article>();
-            articles.Add(new Article(1, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", categories));
-            articles.Add(new Article(2, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", categories));
-            articles.Add(new Article(3, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", categories));
-            articles.Add(new Article(4, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", categories));
+            articles.Add(new Article(1, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", articleCategories));
+            articles.Add(new Article(2, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", articleCategories));
+            articles.Add(new Article(3, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", articleCategories));
+            articles.Add(new Article(4, test, "The Latest: 'El Chapo' Being Taken to Same Prison He Escaped", "The latest on Mexican drug lord Joaquin \"El Chapo\" Guzman who was recaptured six months after he escaped from a maximum security prison: (all times local)", "Mexican Attorney General Ariely Gomez says drug lord Joaquin \"El Chapo\" Guzman is being taken back to Antiplano — the same maximum-security prison where he escaped last July 11 using an elaborate tunnel that was dug to his shower stall.<br><br>Guzman was captured by Mexican marines early Friday in a coastal city, and the attorney general says the drug boss was tracked down partly because he was making a biographical movie.<br><br>The attorney general spoke at a ceremony Friday night at Mexico City's airport where Mexican marines displayed Guzman to journalists.<br>Guzman was put on a navy helicopter to be flown to the prison.", "prision, mexico", "", articleCategories));
+
+            ViewData["Categories"] = categories;
         }
 
         public ActionResult Index()
         {
-            ViewData["Categories"] = categories;
             //Passes the news to the index view
             return View(articles);
         }
@@ -44,26 +54,36 @@ namespace Adow_sOwnBlog.Controllers
         {
             
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
-        public ActionResult showArticle(int id, string title, ArticleCategory category)
+        public ActionResult showArticle(string category, string title, int id)
         {
+            
             return View(articles.FirstOrDefault(x => x.id == id));
         }
 
-        public ActionResult showCategory(int id)
-        {   
-            List<Article> selecCategoryArticles = articles.Where(x => x.id == id).ToList();
-            return View(selecCategoryArticles);
+        public ActionResult showCategory(string category)
+        {
+            List<Article> selectedCategoryArticles = new List<Article>();
+            
+            //Finds all the articles, finds all categories in it and add the
+            //according article to the article list
+            foreach (Article article in articles) {
+                foreach(ArticleCategory cat in article.categories){
+                    if(cat.name==category){
+                        selectedCategoryArticles.Add(article);
+                    }
+                }
+            }
+
+            return View(selectedCategoryArticles);
         }
 
     }
